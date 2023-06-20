7a1c8e44b8ab0cdc590e71837d88130f3b860ce2
ayalasegal <ayalasegal1@gmail.com>
2023-06-20 18:39:49 +0300

    Added validations, solved problems

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./UserRouter');
 const app = express();
 app.use(express.json());
 app.use(userRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,() => {
-    console.console.log('Server running on port ${PORT}');
+    console.log('Server running on port ${PORT}');
 })
