2636d306c6238d5fe69cfcca5f9f9a8faf1888d8
ayalasegal <ayalasegal1@gmail.com>
2023-06-20 17:28:41 +0300

    Changed controller

--- UserController.js
+++ UserController.js
@@ -1,21 +1,108 @@
+// Import the User module or model
+const User = require('./userModel');
 
-const userController = {
-  getAllUsers: async (req, res) => {},
+const controller = {
+  getAllUsers: async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
 
-  getUserById: async (req, res) => {},
+  getUserById: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const user = await User.findById(id);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
 
-  getUserByName: async (req, res) => {},
+  getUserByName: async (req, res) => {
+    const { name } = req.query;
+    try {
+      const user = await User.findOne({ name });
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
 
-  getUserByMail: async (req, res) => {},
+  getUserByMail: async (req, res) => {
+    const { email } = req.query;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
 
-  getEmployeeByPhone: async (req, res) => {},
+  getEmployeeByPhone: async (req, res) => {
+    const { phone } = req.query;
+    try {
+      const user = await User.findOne({ phone });
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
 
-  addNewUser: async (req, res) => {},
+  addNewUser: async (req, res) => {
+    const { name, email, phone } = req.body;
+    try {
+      const newUser = await User.create({ name, email, phone });
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
 
-  updateUser: async (req, res) => {},
+  updateUser: async (req, res) => {
+    const { id } = req.params;
+    const { name, email, phone } = req.body;
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        { name, email, phone },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
 
-  deleteUser: async (req, res) => {},
-  
+  deleteUser: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const deletedUser = await User.findByIdAndDelete(id);
+      if (!deletedUser) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
 };
 
-module.exports = userController;
+module.exports = controller;
