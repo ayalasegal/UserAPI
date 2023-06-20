d52d67ecc51e8154b5c969989e16c892e882dd0b
ayalasegal <ayalasegal1@gmail.com>
2023-06-20 15:07:57 +0300

    added UserController

---
+++ UserController.js
@@ -0,0 +1,28 @@
+
+const userController = {
+  getAllUsers: async (req, res) => {
+  },
+
+  getUserById: async (req, res) => {
+  },
+
+  getUserByName: async (req, res) => {
+  },
+
+  getUserByMail: async (req, res) => {
+  },
+
+  getEmployeeByPhone: async (req, res) => {
+  },
+
+  addNewUser: async (req, res) => {
+  },
+
+  updateUser: async (req, res) => {
+  },
+
+  deleteUser: async (req, res) => {
+  },
+};
+
+module.exports = userController;
