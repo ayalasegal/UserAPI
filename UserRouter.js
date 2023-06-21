761ad8b22783a1a5fb26a2843aff859994307ff9
ChanaLipsky <chanalip7@gmail.com>
2023-06-21 18:34:35 +0300

    Now this is good as far as the eslint library is concerned

--- UserRouter.js
+++ UserRouter.js
@@ -9,6 +9,7 @@ router.get('/users/id/:id', userController.getUserById);
 router.get('/users/name/:name', userController.getUserByName);
 router.get('/users/mail/:email', userController.getUserByMail);
 router.get('/users/phone/:phone', userController.getUserByPhone);
+router.get('/users/date/:birthDate', userController.getUserByBirthDate);
 router.post('/users', userController.addNewUser);
 router.put('/users/:id', userController.updateUser);
 router.delete('/users/:id', userController.deleteUser);
