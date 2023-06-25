fd5d0467b49fd4243e4dcbd36de0e8ba62de0543
ChanaLipsky <chanalip7@gmail.com>
2023-06-25 11:27:52 +0300

    Ready for Code Review!!

--- UserController.js
+++ UserController.js
@@ -124,3 +124,4 @@ const controller = {
 };
 
 module.exports = controller;
+
--- UserRouter.js
+++ UserRouter.js
@@ -15,3 +15,4 @@ router.put('/users/:id', userController.updateUser);
 router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
+
